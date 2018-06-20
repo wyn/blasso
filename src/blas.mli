ed264905b8928e62a38bbf0a5719ebd8258f6189
Simon Parry <simon.parry@coshx.co.uk>
2018-06-20 12:12:38 +0100

    formatting

--- src/blas.mli
+++ src/blas.mli
@@ -1,9 +1,16 @@
 
-val dswap : n:int -> xs:float array -> incx:int -> ys:float array -> incy:int -> unit
+val dswap : n:int ->
+            xs:float array -> incx:int ->
+            ys:float array -> incy:int ->
+            unit
 
-val dscal : n:int -> alpha:float -> xs:float array -> incx:int -> unit
+val dscal : n:int -> alpha:float ->
+            xs:float array -> incx:int ->
+            unit
 
-val drotg : alpha:float ref -> beta:float ref -> c:float ref -> s:float ref -> unit
+val drotg : alpha:float ref -> beta:float ref ->
+            c:float ref -> s:float ref ->
+            unit
 
 val drot : n:int ->
            xs:float array -> incx:int ->
