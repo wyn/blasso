d2cecfe8eddc0e9abb85999d93234fe1285f4222
Simon Parry <simon.parry@coshx.co.uk>
2018-11-14 21:35:43 +0000

    got rid of mli for now

--- src/blas.ml
+++ src/blas.ml
@@ -1,3 +1,73 @@
+module Blas1 : sig
+  
+  val drotg : alpha:float ref -> beta:float ref ->
+              c:float ref -> s:float ref ->
+              unit
+
+  (* val drotmg : alpha:float ref -> beta:float ref ->
+   *              c:float ref -> s:float ref ->
+   *              unit
+   *)
+    
+  val drot : n:int ->
+             xs:float array -> incx:int ->
+             ys:float array -> incy:int ->
+             c:float -> s:float ->
+             unit
+
+  (* val drotm : n:int ->
+   *             xs:float array -> incx:int ->
+   *             ys:float array -> incy:int ->
+   *             c:float -> s:float ->
+   *             unit *)
+
+  val dswap : n:int ->
+              xs:float array -> incx:int ->
+              ys:float array -> incy:int ->
+              unit
+
+  val dscal : n:int -> alpha:float ->
+              xs:float array -> incx:int ->
+              unit
+
+  val dcopy : n:int ->
+              xs:float array -> incx:int ->
+              ys:float array -> incy:int ->
+              unit
+
+  val daxpy : n:int -> alpha:float -> 
+              xs:float array -> incx:int ->
+              ys:float array -> incy:int ->
+              unit
+
+  val ddot : n:int ->
+             xs:float array -> incx:int ->
+             ys:float array -> incy:int ->
+             float
+
+  (* val dsdot : n:int ->
+   *             xs:float array -> incx:int ->
+   *             ys:float array -> incy:int ->
+   *             float *)
+
+  val dnrm2 : n:int ->
+              xs:float array -> incx:int ->
+              float
+    
+  (* val dznrm2 : n:int ->
+   *              xs:float array -> incx:int ->
+   *              float *)
+    
+  val dasum : n:int ->
+              xs:float array -> incx:int ->
+              float
+
+  val idamax : n:int ->
+               xs:float array -> incx:int ->
+               int
+
+end = struct
+  
   let drotg ~alpha ~beta ~c ~s =
     let abs_alpha = abs_float !alpha in
     let abs_beta = abs_float !beta in
@@ -142,3 +212,4 @@
         !idmax
       )
              
+end
