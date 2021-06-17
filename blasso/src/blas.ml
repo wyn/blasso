62121d70e84724dbc21539c5ef7f78b8125e75c6
Simon Parry <simon.parry@karoshibee.com>
2021-06-17 18:52:41 +0100

    moved zipper to own file

--- blasso/src/blas.ml
+++ blasso/src/blas.ml
@@ -1,110 +1,26 @@
-module Zipper = struct
-
-  type el = float
-
-  type t = {
-    left_: el list;
-    focus_: el;
-    right_: el list;
-    index_: int;
-    length_: int;
-  }
-
-  let _clamp bottom top i =
-    let i = min i (top-1) in
-    let i = max i bottom in
-    i
-
-  let get t = t.focus_
-
-  let set {left_; focus_=_; right_; index_; length_} ~value = {
-    left_;
-    focus_=value;
-    right_;
-    index_;
-    length_;
-  }
-
-  let of_array arr ~index =
-    let n = Array.length arr in
-    let i = _clamp 0 n index in {
-      left_=Array.sub arr 0 i
-            |> Array.to_list
-            |> List.rev;
-      focus_=arr.(i);
-      right_=Array.sub arr (i+1) (n-(i+1))
-             |> Array.to_list;
-      index_=i;
-      length_=n;
-    }
-
-  let to_array {left_; focus_; right_; _} =
-    let left_ = left_
-                |> List.rev
-                |> Array.of_list in
-    let right_ = right_
-                 |> Array.of_list in
-    Array.concat [left_; [|focus_|]; right_]
-
-  let shift_left t =
-    match t.left_ with
-    | [] -> t
-    | new_focus_ :: new_left_ ->
-      let new_right_ = t.focus_ :: t.right_ in {
-        left_=new_left_;
-        focus_=new_focus_;
-        right_=new_right_;
-        index_=(t.index_ - 1);
-        length_=t.length_;
-      }
-
-  let shift_right t =
-    match t.right_ with
-    | [] -> t
-    | new_focus_ :: new_right_ ->
-      let new_left_ = t.focus_ :: t.left_ in {
-        left_=new_left_;
-        focus_=new_focus_;
-        right_=new_right_;
-        index_=(t.index_ + 1);
-        length_=t.length_;
-      }
-
-  let rec jump_to t ~index =
-    let n = t.length_ in
-    let new_index = _clamp 0 n index in
-    let i = t.index_ in
-    match compare i new_index with
-    | 0 -> t
-
-    | 1 -> shift_left t
-           |> jump_to ~index:new_index
-
-    | _ -> shift_right t
-           |> jump_to ~index:new_index
-
-
-
-end
-
 (* NOTE
- *
- * always updating xs at one index location with a new value,
- * and also updating the result with the appropriate new calc value/values
- *
- *  If the result hasnt been set (=None) then do full calculation first
- *  and then update *)
+  *
+  * only ever updating xs at one index location at a time
+  * and also updating the result with the appropriate new calc value/values
+  *
+  *  If the result hasnt been set (=None) then do full calculation first
+  *  and then update *)
+
+module Z = Zipper
 
 module Dot_data = struct
+
+  type result = float (* not really a float, its a list of sums of pointwise multiplies *)
+
   type t = {
-    xs_: Zipper.t;
-    ys_: Zipper.t;
-    result_: float option; (* not really a float, its a list of sums of pointwise multiplies *)
+    xs_: Z.t;
+    ys_: Z.t;
+    result_: result option;
   }
 
   let init xs ys = {
-    xs_=xs |> Zipper.of_array ~index:0;
-    ys_=ys |> Zipper.of_array ~index:0;
+    xs_=xs |> Z.of_array ~index:0;
+    ys_=ys |> Z.of_array ~index:0;
     result_=None;
   }
 
@@ -113,19 +29,19 @@ module Dot_data = struct
   let rec update {xs_; ys_; result_} ~index ~value =
     match result_ with
     | Some prev ->
-      let xs_at_i = xs_ |> Zipper.jump_to ~index in
-      let x0 = xs_at_i |> Zipper.get in
-      let ys_at_i = ys_ |> Zipper.jump_to ~index in
-      let y0 = ys_at_i |> Zipper.get in
+      let xs_at_i = xs_ |> Z.jump_to ~index in
+      let x0 = xs_at_i |> Z.get in
+      let ys_at_i = ys_ |> Z.jump_to ~index in
+      let y0 = ys_at_i |> Z.get in
       let result_ = Some (prev +. y0 *. (value -. x0)) in
       {
-        xs_=xs_at_i |> Zipper.set ~value;
+        xs_=xs_at_i |> Z.set ~value;
         ys_=ys_at_i;
         result_;
       }
     | None ->
-      let xarr = xs_ |> Zipper.to_array in
-      let yarr = ys_ |> Zipper.to_array in
+      let xarr = xs_ |> Z.to_array in
+      let yarr = ys_ |> Z.to_array in
       let result_ = Some (
           yarr
           |> Array.map2 (fun x y -> x *. y) xarr
@@ -138,30 +54,38 @@ end
 
 module Swap_data = struct
 
-  type xsys = {
-    xs_: Zipper.t;
-    ys_: Zipper.t;
+  type result = {
+    xs_: Z.t;
+    ys_: Z.t;
   }
 
   type t = {
-    xs_: Zipper.t;
-    ys_: Zipper.t;
-    result_: xsys option;
+    xs_: Z.t;
+    ys_: Z.t;
+    result_: result option;
   }
+
+  let init xs ys = {
+    xs_=xs |> Z.of_array ~index:0;
+    ys_=ys |> Z.of_array ~index:0;
+    result_=None;
+  }
+
+  let result t = t.result_
 
   let rec update {xs_; ys_; result_} ~index ~value =
     match result_ with
     | Some prev ->
       let xs_ = xs_
-                |> Zipper.jump_to ~index
-                |> Zipper.set ~value in
+                |> Z.jump_to ~index
+                |> Z.set ~value in
       let ys_ = ys_
-                |> Zipper.jump_to ~index in
+                |> Z.jump_to ~index in
       let prev_xs = prev.xs_
-                    |> Zipper.jump_to ~index in
+                    |> Z.jump_to ~index in
       let prev_ys = prev.ys_
-                    |> Zipper.jump_to ~index
-                    |> Zipper.set ~value in
+                    |> Z.jump_to ~index
+                    |> Z.set ~value in
       let result_ = Some {
           xs_=prev_xs;
           ys_=prev_ys
@@ -169,35 +93,45 @@ module Swap_data = struct
       {xs_; ys_; result_}
     | None ->
       let result_ = Some {
-          xs_=ys_ |> Zipper.to_array |> Zipper.of_array ~index;
-          ys_=xs_ |> Zipper.to_array |> Zipper.of_array ~index;
+          xs_=ys_ |> Z.to_array |> Z.of_array ~index;
+          ys_=xs_ |> Z.to_array |> Z.of_array ~index;
         } in
       update {xs_; ys_; result_} ~index ~value
 end
 
 module Scale_data = struct
 
+  type result = Z.t
+
   type t = {
-    xs_: Zipper.t;
+    xs_: Z.t;
     alpha_: float;
-    result_: Zipper.t option;
+    result_: result option;
   }
+
+  let init xs alpha = {
+    xs_=xs |> Z.of_array ~index:0;
+    alpha_=alpha;
+    result_=None;
+  }
+
+  let result t = t.result_
 
   let rec update {xs_; alpha_; result_} ~index ~value =
     match result_ with
     | Some prev ->
       let xs_ = xs_
-                |> Zipper.jump_to ~index
-                |> Zipper.set ~value in
-      let chc_at_i = prev |> Zipper.jump_to ~index in
-      let result_ = Some (chc_at_i |> Zipper.set ~value:(value *. alpha_)) in
+                |> Z.jump_to ~index
+                |> Z.set ~value in
+      let chc_at_i = prev |> Z.jump_to ~index in
+      let result_ = Some (chc_at_i |> Z.set ~value:(value *. alpha_)) in
       {xs_; alpha_; result_}
     | None ->
       let result_ = Some (
           xs_
-          |> Zipper.to_array
+          |> Z.to_array
           |> Array.map (fun x -> x *. alpha_)
-          |> Zipper.of_array ~index
+          |> Z.of_array ~index
         ) in
       update {xs_; alpha_; result_} ~index ~value
 
@@ -205,28 +139,37 @@ end
 
 module Copy_data = struct
 
+  type result = Z.t
+
   type t = {
-    xs_: Zipper.t;
-    result_: Zipper.t option;
+    xs_: Z.t;
+    result_: result option;
   }
+
+  let init xs = {
+    xs_=xs |> Z.of_array ~index:0;
+    result_=None;
+  }
+
+  let result t = t.result_
 
   let rec update {xs_; result_} ~index ~value =
     match result_ with
     | Some prev ->
       let xs_ = xs_
-                |> Zipper.jump_to ~index
-                |> Zipper.set ~value in
+                |> Z.jump_to ~index
+                |> Z.set ~value in
       let result_ = Some (
           prev
-          |> Zipper.jump_to ~index
-          |> Zipper.set ~value
+          |> Z.jump_to ~index
+          |> Z.set ~value
         ) in
       {xs_; result_}
     | None ->
       let result_ = Some (
           xs_
-          |> Zipper.to_array
-          |> Zipper.of_array ~index
+          |> Z.to_array
+          |> Z.of_array ~index
         ) in
       update {xs_; result_} ~index ~value
 
---
+++ blasso/src/zipper.ml
@@ -0,0 +1,83 @@
+
+  type el = float
+
+  type t = {
+    left_: el list;
+    focus_: el;
+    right_: el list;
+    index_: int;
+    length_: int;
+  }
+
+  let _clamp bottom top i =
+    let i = min i (top-1) in
+    let i = max i bottom in
+    i
+
+  let get t = t.focus_
+
+  let set {left_; focus_=_; right_; index_; length_} ~value = {
+    left_;
+    focus_=value;
+    right_;
+    index_;
+    length_;
+  }
+
+  let of_array arr ~index =
+    let n = Array.length arr in
+    let i = _clamp 0 n index in {
+      left_=Array.sub arr 0 i
+            |> Array.to_list
+            |> List.rev;
+      focus_=arr.(i);
+      right_=Array.sub arr (i+1) (n-(i+1))
+             |> Array.to_list;
+      index_=i;
+      length_=n;
+    }
+
+  let to_array {left_; focus_; right_; _} =
+    let left_ = left_
+                |> List.rev
+                |> Array.of_list in
+    let right_ = right_
+                 |> Array.of_list in
+    Array.concat [left_; [|focus_|]; right_]
+
+  let shift_left t =
+    match t.left_ with
+    | [] -> t
+    | new_focus_ :: new_left_ ->
+      let new_right_ = t.focus_ :: t.right_ in {
+        left_=new_left_;
+        focus_=new_focus_;
+        right_=new_right_;
+        index_=(t.index_ - 1);
+        length_=t.length_;
+      }
+
+  let shift_right t =
+    match t.right_ with
+    | [] -> t
+    | new_focus_ :: new_right_ ->
+      let new_left_ = t.focus_ :: t.left_ in {
+        left_=new_left_;
+        focus_=new_focus_;
+        right_=new_right_;
+        index_=(t.index_ + 1);
+        length_=t.length_;
+      }
+
+  let rec jump_to t ~index =
+    let n = t.length_ in
+    let new_index = _clamp 0 n index in
+    let curr_index = t.index_ in
+    match compare curr_index new_index with
+    | 0 -> t
+    (* if were are currently at a higher index then need to move left *)
+    | 1 -> shift_left t
+           |> jump_to ~index:new_index
+    (* if we are currently at a lower index then need to move right *)
+    | _ -> shift_right t
+           |> jump_to ~index:new_index
