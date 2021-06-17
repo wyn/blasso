7c7d70d414d5a097a6d2435cdc2e87c4e924b889
Simon Parry <simon.parry@karoshibee.com>
2021-06-17 12:07:56 +0100

    simple zipper

--- blasso/src/blas.ml
+++ blasso/src/blas.ml
@@ -1,8 +1,48 @@
-type 'a array_zipper = {
-  left: 'a array;
-  focus: 'a;
-  right: 'a array
-}
+module Zipper = struct
+
+  type el = float
+
+  type t = {
+    left: el list;
+    focus: el;
+    right: el list
+  }
+
+  let of_array arr i =
+    let n = Array.length arr in
+    let i = min i (n-1) in
+    let i = max i 0 in {
+      left=Array.sub arr 0 i |> Array.to_list |> List.rev;
+      focus=arr.(i);
+      right=Array.sub arr (i+1) (n-(i+1)) |> Array.to_list;
+    }
+
+  let to_array {left; focus; right} =
+    let left = left |> List.rev |> Array.of_list in
+    let right = right |> Array.of_list in
+    Array.concat [left; [|focus|]; right]
+
+  let shift_left {left; focus; right} =
+    match left with
+    | [] -> {left; focus; right}
+    | new_focus :: new_left ->
+      let new_right = focus :: right in {
+        left=new_left;
+        focus=new_focus;
+        right=new_right;
+      }
+
+  let shift_right {left; focus; right} =
+    match right with
+    | [] -> {left; focus; right}
+    | new_focus :: new_right ->
+      let new_left = focus :: left in {
+        left=new_left;
+        focus=new_focus;
+        right=new_right;
+      }
+
+end
 
 type blas_data = {
   xs: float array;
