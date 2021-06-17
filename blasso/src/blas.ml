528015a5719ac075c8d614c7197e7f26b9ab0713
Simon Parry <simon.parry@karoshibee.com>
2021-06-17 11:12:25 +0100

    when cache is None do the dot product first then recurse

--- blasso/src/blas.ml
+++ blasso/src/blas.ml
@@ -10,20 +10,22 @@ type blas_expr =
 
 (* some comments *)
 
-let inc blas_expr i new_value =
+let rec inc blas_expr i new_value =
   match blas_expr with
-  | Dot ({xs; ys; value}) -> (
+  | Dot {xs; ys; value} -> (
       match value with
       | Some prev ->
         let x0 = xs.(i) in
         let y0 = ys.(i) in
         let curr = prev +. y0 *. (new_value -. x0) in
         let () = xs.(i) <- new_value in
-        Dot ({xs; ys; value=(Some curr)})
-      | None -> Dot ({xs; ys; value})
+        Dot {xs; ys; value=(Some curr)}
+      | None ->
+        let value = Some (Array.map2 (fun x y -> x *. y) xs ys |> Array.fold_left (fun acc v -> acc +. v) 0.) in
+        inc (Dot {xs; ys; value}) i new_value
     )
-  | Swap ({xs; ys; value}) ->
+  | Swap {xs; ys; value} ->
     let tmp = Array.get xs i in
     let () = xs.(i) <- ys.(i) in
     let () = ys.(i) <- tmp in
-    Swap ({xs; ys; value})
+    Swap {xs; ys; value}
