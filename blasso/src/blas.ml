9c72ecd0025a2423406dcb824d6a625303ee63fb
Simon Parry <simon.parry@karoshibee.com>
2021-06-17 12:50:39 +0100

    more zipper funcs

--- blasso/src/blas.ml
+++ blasso/src/blas.ml
@@ -3,44 +3,71 @@ module Zipper = struct
   type el = float
 
   type t = {
-    left: el list;
-    focus: el;
-    right: el list
+    left_: el list;
+    focus_: el;
+    right_: el list;
+    index_: int;
+    length_: int;
   }
+
+  let _clamp bottom top i =
+    let i = min i (top-1) in
+    let i = max i bottom in
+    i
+
+  let get t = t.focus_
+
+  let set {left_; focus_=_; right_; index_; length_} value = {left_; focus_=value; right_; index_; length_}
 
   let of_array arr i =
     let n = Array.length arr in
-    let i = min i (n-1) in
-    let i = max i 0 in {
-      left=Array.sub arr 0 i |> Array.to_list |> List.rev;
-      focus=arr.(i);
-      right=Array.sub arr (i+1) (n-(i+1)) |> Array.to_list;
+    let i = _clamp 0 n i in {
+      left_=Array.sub arr 0 i |> Array.to_list |> List.rev;
+      focus_=arr.(i);
+      right_=Array.sub arr (i+1) (n-(i+1)) |> Array.to_list;
+      index_=i;
+      length_=n;
     }
 
-  let to_array {left; focus; right} =
-    let left = left |> List.rev |> Array.of_list in
-    let right = right |> Array.of_list in
-    Array.concat [left; [|focus|]; right]
+  let to_array {left_; focus_; right_; _} =
+    let left_ = left_ |> List.rev |> Array.of_list in
+    let right_ = right_ |> Array.of_list in
+    Array.concat [left_; [|focus_|]; right_]
 
-  let shift_left {left; focus; right} =
-    match left with
-    | [] -> {left; focus; right}
-    | new_focus :: new_left ->
-      let new_right = focus :: right in {
-        left=new_left;
-        focus=new_focus;
-        right=new_right;
+  let shift_left t =
+    match t.left_ with
+    | [] -> t
+    | new_focus_ :: new_left_ ->
+      let new_right_ = t.focus_ :: t.right_ in {
+        left_=new_left_;
+        focus_=new_focus_;
+        right_=new_right_;
+        index_=(t.index_ - 1);
+        length_=t.length_;
       }
 
-  let shift_right {left; focus; right} =
-    match right with
-    | [] -> {left; focus; right}
-    | new_focus :: new_right ->
-      let new_left = focus :: left in {
-        left=new_left;
-        focus=new_focus;
-        right=new_right;
+  let shift_right t =
+    match t.right_ with
+    | [] -> t
+    | new_focus_ :: new_right_ ->
+      let new_left_ = t.focus_ :: t.left_ in {
+        left_=new_left_;
+        focus_=new_focus_;
+        right_=new_right_;
+        index_=(t.index_ + 1);
+        length_=t.length_;
       }
+
+  let rec jump_to t ~new_index =
+    let n = t.length_ in
+    let new_index = _clamp 0 n new_index in
+    let i = t.index_ in
+    match compare i new_index with
+    | 0 -> t
+    | 1 -> let t = shift_left t in jump_to t ~new_index
+    | _ -> let t = shift_right t in jump_to t ~new_index
+
+
 
 end
 
