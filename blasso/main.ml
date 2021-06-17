5285a8c74739ddb85ed9b129844112909b46737d
Simon Parry <simon.parry@karoshibee.com>
2021-06-17 11:12:18 +0100

    updated main

--- blasso/main.ml
+++ blasso/main.ml
@@ -5,9 +5,9 @@ let _ =
   let x1 = [| 0.; 1.; 2.; 3.; 400.; 5.; 6.; 7.; 8.; 9. |] in
   let y0 = Array.map (fun y -> 2. *. y) (Array.copy x0) in
   let s0 = 570. in
-  let d = B.Dot (x0, y0, s0) in
+  let d = B.Dot {xs=x0; ys=y0; value=(Some s0)} in
   let s1 = match B.inc d 4 (Array.get x1 4) with
-    | B.Dot (_, _, s1) -> s1
+    | B.Dot {value=Some v; _} -> v
     | _ -> raise (Sys_error "Unknown Blas op")
   in
   let ss = Printf.sprintf "s0: %f, s1: %f, 3738." s0 s1 in
