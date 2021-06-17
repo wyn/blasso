db656e54d9730809ddf6f6768d928ffa238ee539
Simon Parry <simon.parry@karoshibee.com>
2021-06-17 10:54:52 +0100

    changed to blas_data

--- blasso/main.ml
+++ blasso/main.ml
@@ -1,12 +1,14 @@
+module B = Blasso.Blas
+
 let _ =
   let x0 = [| 0.; 1.; 2.; 3.; 4.; 5.; 6.; 7.; 8.; 9. |] in
   let x1 = [| 0.; 1.; 2.; 3.; 400.; 5.; 6.; 7.; 8.; 9. |] in
   let y0 = Array.map (fun y -> 2. *. y) (Array.copy x0) in
   let s0 = 570. in
-  let d = Blasso.Blas.Dot (x0, y0, s0) in
-  let s1 = match Blasso.Blas.inc d 4 (Array.get x1 4) with
-    | Blasso.Blas.Dot (_, _, s1) -> s1
-    | _ -> -1.
+  let d = B.Dot (x0, y0, s0) in
+  let s1 = match B.inc d 4 (Array.get x1 4) with
+    | B.Dot (_, _, s1) -> s1
+    | _ -> raise (Sys_error "Unknown Blas op")
   in
   let ss = Printf.sprintf "s0: %f, s1: %f, 3738." s0 s1 in
   print_endline ss
--- blasso/src/blas.ml
+++ blasso/src/blas.ml
@@ -1,21 +1,29 @@
+type blas_data = {
+  xs: float array;
+  ys: float array;
+  value: float option
+}
 
 type blas_expr =
-  | Dot of float array * float array * float
-  | Swap of float array * float array
+  | Dot of blas_data
+  | Swap of blas_data
 
 (* some comments *)
 
 let inc blas_expr i new_value =
   match blas_expr with
-  | Dot (xs, ys, prev) ->
-    let x0 = xs.(i) in
-    let y0 = ys.(i) in
-    let curr = prev +. y0 *. (new_value -. x0) in
-    let () = xs.(i) <- new_value in
-    Dot (xs, ys, curr)
-
-  | Swap (xs, ys) ->
+  | Dot ({xs; ys; value}) -> (
+      match value with
+      | Some prev ->
+        let x0 = xs.(i) in
+        let y0 = ys.(i) in
+        let curr = prev +. y0 *. (new_value -. x0) in
+        let () = xs.(i) <- new_value in
+        Dot ({xs; ys; value=(Some curr)})
+      | None -> Dot ({xs; ys; value})
+    )
+  | Swap ({xs; ys; value}) ->
     let tmp = Array.get xs i in
     let () = xs.(i) <- ys.(i) in
     let () = ys.(i) <- tmp in
-    Swap (xs, ys)
+    Swap ({xs; ys; value})
